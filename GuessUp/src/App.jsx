9a8f7a39b60dfd4e72a3bb829c967fd2a0d4a2ae
Simon <simon.dominik@hbsz.edu.hu>
2026-04-06 19:10:00 +0200

    setup 1.2

--- GuessUp/src/App.jsx
+++ GuessUp/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Mainpage from "./assets/MainPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
