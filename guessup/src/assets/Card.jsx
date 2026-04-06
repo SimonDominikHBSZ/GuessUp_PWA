dd23e0fe12bb6ce35333ea740c007b543bf2970d
Simon <simon.dominik@hbsz.edu.hu>
2026-04-06 21:25:18 +0200

    basic cards

---
+++ guessup/src/assets/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({ title, subtitle }) {
+  return (
+    <div id="Card">
+      <h1>{title}</h1>
+      <h3>{subtitle}</h3>
+    </div>
+  );
+}
--- guessup/src/assets/Categories.jsx
+++ guessup/src/assets/Categories.jsx
@@ -1,8 +1,16 @@
+import Card from "./Card";
+
 export default function Categories() {
   return (
-    <div>
-      <div>fejlec</div>
-      <div>main</div>
+    <div id="Category-cont">
+      <div id="Category-header">
+        <h1>Guess UP</h1>
+      </div>
+      <div id="Category-body">
+        <Card title="test" subtitle="EzEgyTeszt" />
+        <Card title="test" subtitle="EzEgyTeszt" />
+        <Card title="test" subtitle="EzEgyTeszt" />
+      </div>
     </div>
   );
 }
